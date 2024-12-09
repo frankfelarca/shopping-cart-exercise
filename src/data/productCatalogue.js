dcb0f59769bb748f7af94c6a86e6fc680ff26e82
Frank <frankfelarca@gmail.com>
2024-12-09 17:23:37 +0800

    feat: add product catalogue data

---
+++ src/data/productCatalogue.js
@@ -0,0 +1,6 @@
+export const productCatalogue = {
+  ult_small: { name: "Unlimited 1GB", price: 24.9 },
+  ult_medium: { name: "Unlimited 2GB", price: 29.9 },
+  ult_large: { name: "Unlimited 5GB", price: 44.9 },
+  "1gb": { name: "1 GB Data-pack", price: 9.9 },
+};
