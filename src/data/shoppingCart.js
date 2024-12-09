f49c5bc4391efbf395c11c1926d0c3fbd614301b
Frank <frankfelarca@gmail.com>
2024-12-09 20:33:46 +0800

    feat: shopping cart function

---
+++ src/data/shoppingCart.js
@@ -0,0 +1,48 @@
+import { productCatalogue } from "./productCatalogue.js";
+
+export const ShoppingCart = () => {
+  // Cart state
+  let cartItems = [];
+  let promoCode = null;
+
+  return {
+    // Initializes the cart.
+    new: (pricingRules) => {
+      return {
+        // Add items to the cart.
+        add: (itemCode, code = null) => {
+          const product = productCatalogue[itemCode];
+          if (product) {
+            cartItems.push({ code: itemCode, ...product });
+            if (code) {
+              promoCode = code;
+            }
+          } else {
+            throw new Error(`Item ${itemCode} not found in catalog.`);
+          }
+        },
+
+        // Returns the total price of the cart.
+        total: () => {
+          const { total } = pricingRules.calculatePrice(cartItems, promoCode);
+          console.log("Total: ", total);
+          return parseFloat(total);
+        },
+
+        // Returns the list of items in the cart.
+        items: () => {
+          const { items } = pricingRules.calculatePrice(cartItems, promoCode);
+          console.log("Items: ", items);
+          console.log("\n");
+          return items;
+        },
+
+        // Clear the items in the cart.
+        clear: () => {
+          cartItems = [];
+          promoCode = null;
+        },
+      };
+    },
+  };
+};
